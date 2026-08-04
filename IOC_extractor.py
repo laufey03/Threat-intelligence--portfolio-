ded96b79f544a6a3747d3758b7fe072ed69148fa
laufey <rakeshradhakrishnancyber@gmail.com>
2026-08-04 12:53:43 +0000

    IP and DOMAIN extraction pattern

--- IOC_extractor.py
+++ IOC_extractor.py
@@ -6,6 +6,7 @@ ip_pattern = re.compile(r'\b((\d{1,3}\.){3}\d{1,3})\b')
 
 #text containing IOCs 
 test_test = """ Malicious activity was observed from 192.168.1.1 and 45.33.32.156 attempting to connect to 10.0.0.1 .
+The domains mailicious-site.com and sub.badactor.xyz were used as command and control infrastructure.
 no activity from 999.999.999.999."""
 
 #search for IP address in test_test
@@ -15,3 +16,10 @@ found_ips = ip_pattern.findall(test_test)
 print("IP adresses found:")
 for ip in found_ips:
     print(ip[0])
+
+domain_pattern = re.compile(r'\b((?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6})\b')
+found_domains=domain_pattern.findall(test_test)
+print("Domains Found:")
+for domain in found_domains:
+    print(domain)
+
