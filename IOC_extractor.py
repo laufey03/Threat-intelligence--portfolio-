61a76ab29ba44b3b66eec693a5730599ae845973
laufey <rakeshradhakrishnancyber@gmail.com>
2026-08-10 16:04:09 +0000

    A complete working code

--- IOC_extractor.py
+++ IOC_extractor.py
@@ -1,7 +1,7 @@
 # Step 1 importing module and loading the extractor 
 import re 
 
-#text containing IOCs 
+#text containing IOCs code testing 
 #test_test = """ Malicious activity was observed from 192.168.1.1 and 45.33.32.156 attempting to connect to 10.0.0.1 .
 #The domains mailicious-site.com and sub.badactor.xyz were used as command and control infrastructure. The payload was downloaded from https://malicious-site.com/payload/file.exe
 #and http://sub.badactor.xyz/download?id=123 was used for data exfiltration.MD5 hash: d41d8cd98f00b204e9800998ecf8427e
@@ -24,6 +24,10 @@ while True:
     lines.append(line)
 text = "\n".join(lines)
 
+print("\n =======================================================================================================")
+print("\n                                     IOC EXTRACTION RESULTS                  ")
+print("\n =======================================================================================================")
+
 # Step 2 - IP address Pattern
 ip_pattern = re.compile(r'\b((?:\d{1,3}\.){3}\d{1,3})\b')
 
@@ -31,33 +35,33 @@ ip_pattern = re.compile(r'\b((?:\d{1,3}\.){3}\d{1,3})\b')
 found_ips = ip_pattern.findall(text)
 
 #Display results 
-print("IP adresses found:")
+print(f"\n   [+] IP adresses found: {len(found_ips)}")
 for ip in found_ips:
-    print(ip)
+    print(f"   ->  {ip}")
 
 #domain_pattern
 
 domain_pattern = re.compile(r'(?<![:/])\b((?:[a-zA-Z0-9-]+\.)+(?:com|org|net|edu|gov|mil|xyz|io|co|uk|de|fr|ru|cn|jp))\b')
 found_domains=domain_pattern.findall(text)
-print("Domains Found:")
+print(f"\n   [+] Domains Found:{len(found_domains)}")
 for domain in found_domains:
-    print(domain)
+    print(f"   ->  {domain}")
 
 #url_pattern
 url_pattern = re.compile(r'(https?://(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(?:/[^\s]*)?)')
 #search for urls
 found_urls=url_pattern.findall(text)
-print("\n Found URLs:")
+print(f"\n   [+] Found URLs:{len(found_urls)}")
 for url in found_urls:
-    print(url)
+    print(f"   -> {url}")
 
 #hash_pattern
 hash_pattern= re.compile(r'\b([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64}|[a-fA-F0-9]{128})\b')
 
 #search_hashes
 found_hashes= hash_pattern.findall(text)
-print("\n Files Hashes found:")
+print("\n   [+] Files Hashes found:{len(found_hashes)}")
 for hash in found_hashes:
-    print(hash)
+    print(f"   -> {hash}")
 
-
+print("\n ======================================================================================================")
