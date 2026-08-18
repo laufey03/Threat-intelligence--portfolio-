d9e94a1c82a3bd61b50e9867cf872f055890c889
laufey <rakeshradhakrishnancyber@gmail.com>
2026-08-18 10:40:43 +0000

    phising analyzer tool under development

---
+++ 05-Phishing-analyzer/phishing_analyzer.py
@@ -0,0 +1,7 @@
+import re
+import email 
+
+print ("\n Phishing Email Header analyzer loaded sucessfully.........")
+print("\n Paste the Raw Email header below ....")
+print("\n Press enter twice when done :")
+
