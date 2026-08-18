0e69ec31bac5453a2d8dbbac48dbb71dd707a27b
laufey <rakeshradhakrishnancyber@gmail.com>
2026-08-18 11:13:35 +0000

    The tool is under development

--- 05-Phishing-analyzer/phishing_analyzer.py
+++ 05-Phishing-analyzer/phishing_analyzer.py
@@ -5,3 +5,20 @@ print ("\n Phishing Email Header analyzer loaded sucessfully.........")
 print("\n Paste the Raw Email header below ....")
 print("\n Press enter twice when done :")
 
+lines = []
+while True:
+    line = input()
+    if line == "":
+        break
+    lines.append(line)
+raw_header = "\n".join(lines)
+
+msg= email.message_from_string(raw_header)
+
+from_address = msg.get('From', 'Not found')
+reply_to = msg.get('Reply-To', 'Not found')
+return_path = msg.get('Return-Path', 'Not found')
+subject = msg.get('Subject', 'Not found')
+date = msg.get('Date', 'Not found')
+
+ 
