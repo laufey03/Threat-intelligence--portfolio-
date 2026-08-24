67ddedc8a436fc5b50e7df2afff3b9c0c22e4e34
laufey <rakeshradhakrishnancyber@gmail.com>
2026-08-24 14:11:43 +0000

    The Phishing Email Header analyzer

--- 05-Phishing-analyzer/phishing_analyzer.py
+++ 05-Phishing-analyzer/phishing_analyzer.py
@@ -21,15 +21,18 @@ return_path = msg.get('Return-Path', 'Not found')
 subject = msg.get('Subject', 'Not found')
 date = msg.get('Date', 'Not found')
 
-auth_results = msg.get('Authenctication-Result', 'Not found')
+auth_results = ""
+for line in lines:
+    if 'Authentication-Results' in line or "spf=" in line.lower() or "dkim=" in line.lower() or "dmarc=" in line.lower():
+        auth_results += ''
 
-spf_match = re.search(r'spf=(\w+)', auth_results)
+spf_match = re.search(r'spf=(\w+)', auth_results.lower())
 spf_result = spf_match.group(1) if spf_match else 'Not found'
 
-dkim_match = re.search(r'dkim=(\w+)', auth_results)
+dkim_match = re.search(r'dkim=(\w+)', auth_results.lower())
 dkim_result = dkim_match.group(1) if dkim_match else 'Not found'
 
-dmarc_match = re.search(r'dmarc=(\w+)', auth_results)
+dmarc_match = re.search(r'dmarc=(\w+)', auth_results.lower())
 dmarc_result = dmarc_match.group(1) if dmarc_match else 'Not found'
 
 
@@ -39,7 +42,7 @@ print("\n ======================================================================
 print(f"\n        Subject         :  {subject}")
 print(f"\n        Date            :  {date}")
 print(f"\n        From            :  {from_address}")
-print(f"\n        Reply-To        :  {reply_to}}")
+print(f"\n        Reply-To        :  {reply_to}")
 print(f"\n        Return-Path     :  {return_path}")
 
 print(f"\n                               --------AUTHENTICATION RESULTS-------------                                     ")
@@ -60,7 +63,7 @@ if dmarc_result == "fail":
 if reply_to != "Not found" and reply_to != from_address:
     print(f"\n ! Reply-To Mismatch From address : {from_address} but Reply-To : {reply_to} ")
 
-if spf_result != "fail" and dkim_result != "fail" and dmarc_result != "fail" and (reply_to == "Not found" or reply_to == from_address)
+if spf_result != "fail" and dkim_result != "fail" and dmarc_result != "fail" and (reply_to == "Not found" or reply_to == from_address):
     print("   [+] NO SUSPICIOUS INDICATORS DETECTED ........")
 
 print("\n ===============================================================================================================")
